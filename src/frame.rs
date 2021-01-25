38c73ae8187a1698782af6917c18f719442a0bc1
parkcheolu <myqueen1202@gmail.com>
2021-01-25 17:35:38 +0900

    initial commit frame.rs

--- src/frame.rs
+++ src/frame.rs
@@ -1,5 +1,6 @@
+//! Redis 프로토콜 프레임을 표현하는 타입과 바이트 배열로부터 프레임을 파싱하기 위한 유틸리치를 제공한다.
 
-
+// Redis 프로토콜의 프레임.
 pub enum Frame {
     Simple(String),
     Error(String),
@@ -10,15 +11,25 @@ pub enum Frame {
 }
 
 pub enum Error {
+    // 메시지 파싱을 위한 데이터가 부족함을 의미한다.
     Incomplete,
+    // 유효하지 않은 메시지 인코딩을 의미한다.
     Other(crate::Error),
 }
 
 impl Frame {
+    // 빈 배열 프레임을 반환한다.
     pub(crate) fn array() -> Frame {
         Frame::Array(vec![])
     }
 
+    /**
+     * "bulk" 프레임을 배열에 넣는다. 'self'는 반드시 배열 프레임이어야 한다.
+     * 
+     * # Panics
+     * 
+     * 'self'가 배열이 아닌 경우 패닉.
+     */
     pub(crate) fn push_bulk(&mut self, bytes: Bytes) {
         match self {
             Frame::Array(vec) => {
@@ -28,6 +39,13 @@ impl Frame {
         }
     }
 
+    /**
+     * "integer" 프레임을 배열에 넣는다. 'self'는 반드시 배열 프레임이어야 한다.
+     * 
+     * # Panics
+     * 
+     * 'self'가 배열이 아닌 경우 패닉.
+     */    
     pub(crate) fn push_int(&mut self, value: u64) {
         match self {
             Frame::Array(vec) => {
@@ -37,6 +55,7 @@ impl Frame {
         }
     }
 
+    // 'src'로부터의 전체 메시지가 디코딩될 수 있는지 확인한다.
     pub fn check(src: &mut Cursor<&[u8]>) -> Result<(), Error> {
         match get_u8(src)? {
             b'+' => {
@@ -53,9 +72,13 @@ impl Frame {
             }
             b'$' => {
                 if b'-' == peek_u8(src)? {
+                    // '-1\r\n'은 생략한다.
                     skip(src, 4)
                 } else {
+                    // 벌크 문자열을 읽는다.
                     let len: usize = get_decimal(src)?.try_into()?;
+
+                    // 바이트 + 2(\r\n) 의 수만큼 생략한다.
                     skip(src, len + 2)
                 }
             }
@@ -69,6 +92,71 @@ impl Frame {
             }
             actual => Err(format!("protocol error; invalid frame type byte '{}'", actual).into()),
         }
+    }
+
+    // 메시지는 'check'를 통해 이미 검증되었다.
+    pub fn parse(src: &mut Cursor<&[u8]>) -> Result<Frame, Error> {
+        match get_u8(src)? {
+            b'+' => {
+                // 라인을 읽어 'Vec<u8>'으로 변환한다.
+                let line = get_line(src)?.to_vec();
+
+                // 라인을 문자열로 변환한다.
+                let string = String::from_utf8(line)?;
+
+                Ok(Frame::Simple(string))
+            }
+            b'-' => {
+                let line = get_line(src)?.to_vec();
+                let string = String::from_utf8(line)?;
+                Ok(Frame::Error(string))
+            }
+            b':' => {
+                let len = get_decimal(src)?;
+                Ok(Frame::Integer(len))
+            }
+            b'$' => {
+                if b'-' == peek_u8(src)? {
+                    let line = get_line(src)?;
+                    if line != b"-1" {
+                        return Err("protocol error; invalid frame format".into());
+                    }
+
+                    Ok(Frame::Null)
+                } else {
+                    // 벌크 문자열을 읽는다.
+                    let len = get_decimal(src)?.try_into()?;
+                    let n = len + 2;
+
+                    if src.remainig() < n {
+                        return Err(Error::Incomplete);
+                    }
+
+                    let data = Bytes::copy_from_slice(&src.chunk()[..len]);
+
+                    // 바이트 + 2(\r\n) 의 수만큼 생략한다.
+                    skip(src, n)?;
+
+                    Ok(Frame::Bulk(data))
+                }
+            }
+            b'*' => {
+                let len = get_decimal(src)?.try_into()?;
+                let mut out = Vec::with_capacity(len);
+
+                for _ in 0..len {
+                    out.push(Frame::parse(src)?);
+                }
+
+                Ok(Frame::Array(out))
+            }
+            _ => unimplemented!(),
+        }
+    }
+
+    // 프레임을 "unexpected frame" 에러 프레임으로 변환한다.
+    pub(crate) fn to_error(&self) -> crate::Error {
+        format!("unexpected frame: {}", self).into()
     }
 }
 
@@ -134,6 +222,8 @@ fn skip(src: &mut Cursor<&[u8]>, n: usize) -> Result<(), Error> {
     Ok(())
 }
 
+
+// 십진수로 끝나는 새로운 라인을 읽는다.
 fn get_decimal(src: &mut Cursor<&[u8]>) -> Result<u64, Error> {
     use atoi::atoi;
 
@@ -142,14 +232,19 @@ fn get_decimal(src: &mut Cursor<&[u8]>) -> Result<u64, Error> {
     atoi::<u64>(line).ok_or_else(|| "protocol error; invalid frame format".into())
 }
 
+// 라인을 찾는다.
 fn get_line<'a>(src: &mut Cursor<&'a [u8]>) -> Result<&'a [u8], Error> {
+    // 바이트를 직접 스캔한다.
     let start = src.position() as usize;
-
+    // 마지막 바이트까지 스캔한다.
     let end - src.get_ref().len() - 1;
 
     for i in start..end {
         if src.get_ref()[i] == b'\r' && src.get_ref()[i + 1] == b'\n' {
+            // 라인을 찾으면 포지션을 \n 뒤로 이동시킨다.
             src.set_position((i + 2) as u64);
+
+            // 라인을 반환한다.
             return Ok(&src.get_ref()[start..i]);
         }
     }
