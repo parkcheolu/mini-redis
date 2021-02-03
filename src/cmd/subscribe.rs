621c9fbb973dbbce80a9a833394f3ebbe47d490e
parkcheolu <myqueen1202@gmail.com>
2021-02-03 23:20:14 +0900

    subscribe.rs in progress

--- src/cmd/subscribe.rs
+++ src/cmd/subscribe.rs
@@ -133,12 +133,14 @@ impl Subscribe {
              * - 서버 셧다운 시그널
              */
             select! {
+                // 구독 채널로부터 메시지를 수신한다.
                 Some((channel_name, msg)) = subscriptions.next() => {
                     dst.write_frame(&make_message_frame(channel_name, msg)).await?;
                 }
                 res = dst.read_frame() => {
                     let frame = match res? {
                         Some(frame) => frame,
+                        // 원격 클라이언트의 연결이 끊어지면 발생한다.
                         none => return Ok(())
                     };
 
@@ -179,8 +181,107 @@ async fn subscribe_to_channel(
 ) -> crate::Result<()> {
     let mut rx = db.subscribe(channel_name.clone());
 
+    // 채널을 구독한다.
     let rx = Box::pin(async_stream::stream! {
-        
+        loop {
+            match rx.recv().await {
+                Ok(msg) => yield msg,
+                // 메시지 소비에서 지연이 발생하면 그냥 다시 시도한다.
+                Err(broadcast::error::RecvError::Lagged(_)) => {}
+                Err(_) => break,
+            }
+        }
     });
+
+    // 클라이언트의 구독 목록 안의 구독을 추적한다.
+    subscriptions.insert(channel_name.clone(), rx);
+
+    // 성공적으로 구독을 마쳤음을 응답한다.
+    let response = make_subscribe_frame(channel_name, subscriptions.len());
+    dst.write_frame(&response).await?;
+
+    Ok(())
 }
+
+/**
+ * 'Subscribe::apply'에 있는 동안 수신한 커맨드를 핸들링한다. 이 시점에는 구독과 해지
+ * 커맨드만이 허용된다.
+ * 
+ * 다른 새로운 구독은 'subscriptions'를 변경하는 대신 'subscribe_to'에 추가된다.
+ */
+async fn handle_command(
+    frame: Frame,
+    subscribe_to: &mut Vec<String>,
+    subscriptions: &mut StreamMap<String, Messages>,
+    dst: &mut Connection,
+) -> crate::Result<()> {
+    /**
+     * 클라이언트로부터 수신한 커맨드
+     * 
+     * 여기서는 'SUBSCRIBE', 'UNSUBSCRIBE' 커맨드만이 허용된다.
+     */
+    match Command::from_frame(frame)? {
+        Command::Subscribe(subscribe) => {
+            // 여기서 vector에 추가한 채널을 'apply' 메서드에서 구독한다.
+            subscribe_to.extend(subscribe.channels.into_iter());
+        }
+        Command::Unsubscribe(mut unsubscribe) => {
+            /**
+             * 채널이 지정되지 않았다면 이 요청은 모든 채널을 구독 해지한다.
+             * 이를 구현하기 위해 현재 구독 중인 채널 목록을 'unsubscribe.channels'의
+             * vector에 위치시킨다.
+             */
+            if unsubscribe.channels.is_empty() {
+                unsubscribe.channels = subscriptions
+                    .keys()
+                    .map(|channel_name| channel_name.to_string())
+                    .collect();
+            }
+
+            for channel_name in unsubscribe.channels {
+                subscriptions.remove(&channel_name);
+
+                let response = make_unsubscribe_frame(channel_name, subscriptions.len());
+                dst.write_frame(&response).await?;
+            }
+        }
+        Command => {
+            let cmd = Unknown::new(command.get_name());
+            cmd.apply(dst).await?;
+        }
+    }
+    Ok(())
+}
+
+/**
+ * 구독 요청에 대한 응답을 생성한다.
+ * 
+ * 'Bytes::from'은 'String' 안의 할당을 재활용할 수 있고, '&str'은 데이터 복사를
+ * 요구하기 때문에, 이들 함수는 'channel_name'을 '&str'이 아닌, 'String'으로 취한다.
+ * 이렇게 하여 함수 호출자는 채널 이름을 clone할 것인지 아닌지 결정할 수 있다.
+ */
+fn make_subscribe_frame(channel_name: String, num_subs: usize) -> Frame {
+    let mut response = Frame::array();
+    response.push_bulk(Bytes::from_static(b"subscribe"));
+    response.push_bulk(Bytes::from(channel_name));
+    response.push_int(num_subs as u64);
+    response
+}
+
+// 구독 해지 요청에 대한 응답을 생성한다.
+fn make_unsubscribe_frame(channel_name: String, num_subs: usize) -> Frame {
+    let mut response = Frame::array();
+    response.push_bulk(Bytes::from_static(b"unsubscribe"));
+    response.push_bulk(Bytes::from(channel_name));
+    response.push_int(num_subs as u64);
+    response
+}
+
+// 클라이언트에게, 구독 중인 채널에서 메시지가 수신되었음을 알리는 메시지를 생성한다.
+fn make_message_frame(channel_name: String, msg: Bytes) -> Frame {
+    
+}
+
+
+
 
